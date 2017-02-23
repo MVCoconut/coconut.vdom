28e6372e3a4c93bbe45f499071059fc5e83e7717
back2dos <back2dos@gmail.com>
2017-02-23 12:39:36 +0100

    Improved caching.

--- src/coconut/vdom/ViewBase.hx
+++ src/coconut/vdom/ViewBase.hx
@@ -3,19 +3,26 @@ package coconut.vdom;
 import tink.state.Observable;
 
 class ViewBase<Original, Presented> extends Renderable {
-
   @:noCompletion var __lastPresented:Presented;
-
-  public function new(data:Original, extract:Original->Presented, renderer:Presented->vdom.VNode, key) {
+  
+  public function new(data:Original, extract:Original->Presented, renderer:Presented->vdom.VNode, key:vdom.Attr.Key) {
     super(Observable.auto(function () {
+      __beforeExtract();
       var nu = extract(data);
       return 
-        if (__lastPresented != null && compare(nu, __lastPresented)) last;
+        if (__lastPresented != null && __compare(nu, __lastPresented)) last;
         else renderer(__lastPresented = nu);
     }), key);
   }
 
-  @:noCompletion private function compare(nu:Presented, old:Presented) {
+  @:noCompletion private function __beforeExtract() {}
+  @:noCompletion private function __resetCache<A>(?v:A) 
+    this.__lastPresented = null;
+
+  @:noCompletion private function __copyCache(old:ViewBase<Original, Presented>) 
+    this.__lastPresented = old.__lastPresented;
+
+  @:noCompletion private function __compare(nu:Presented, old:Presented) {
     if (nu == old) return true;
 
     for (f in Reflect.fields(nu)) {
@@ -27,7 +34,8 @@ class ViewBase<Original, Presented> extends Renderable {
           case [null, _] | [_, null]: 
             return false;
           case [a, b]: 
-            return a.m.value == b.m.value;
+            if (a.m.value != b.m.value)
+              return false;
         }
     }
     return true;
