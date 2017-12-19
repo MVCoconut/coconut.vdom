7c799c90d456ff0e5b5394d31ff990c5fa989733
Juraj Kirchheim <back2dos@googlemail.com>
2017-12-20 00:09:32 +0100

    Add missing cast.

--- src/coconut/vdom/RenderResult.hx
+++ src/coconut/vdom/RenderResult.hx
@@ -1,5 +1,5 @@
 package coconut.vdom;
 
-@:coreType @:pure abstract RenderResult from vdom.VNode from vdom.Widget from String from Int from Float from Bool from Array<RenderResult> to vdom.VNode {
+@:coreType @:pure abstract RenderResult from vdom.VNode from Array<vdom.VNode> from vdom.Widget from String from Int from Float from Bool from Array<RenderResult> to vdom.VNode {
 
 }
