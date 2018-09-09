656cb5bbf38d7f2805c1653638bfba40a5bb2120
Juraj Kirchheim <back2dos@googlemail.com>
2018-09-09 09:20:18 +0200

    Use classname from tink_domspec.

--- src/coconut/vdom/ClassName.hx
+++ src/coconut/vdom/ClassName.hx
@@ -1,21 +1,3 @@
 package coconut.vdom;
 
-abstract ClassName(String) to String {
-  
-  inline function new(s:String) this = s;
-  
-  public function add(that:ClassName) 
-    return new ClassName(switch [this, (that:String)] {
-      case [null, v] | [v, null]: v;
-      case [a, b]: '$a $b';
-    });
-
-  @:from static function ofMap(parts:Map<String, Bool>)
-    return new ClassName(ofArray([for (c in parts.keys()) if (parts[c]) ofString(c)]));
-  
-  @:from static function ofArray(parts:Array<String>) 
-    return new ClassName(parts.map(ofString).join(' '));
-
-  @:from static function ofString(s:String):ClassName
-    return new ClassName(untyped s.trim());
-}
+typedef ClassName = tink.domspec.ClassName;
