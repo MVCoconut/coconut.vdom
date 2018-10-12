b946188fb5f519f44c2fdbdfd95e83ea84569dbd
Juraj Kirchheim <back2dos@googlemail.com>
2018-10-13 00:10:19 +0200

    It would seem to work.

--- src/coconut/vdom/Child.hx
+++ src/coconut/vdom/Child.hx
@@ -9,6 +9,7 @@ typedef VDom = {
   var children:coconut.ui.Children;
 }
 
+@:pure
 abstract Child(VNode<VDom, Node>) to VNode<VDom, Node> {
   
   inline function new(n) this = n;
@@ -38,51 +39,79 @@ abstract Child(VNode<VDom, Node>) to VNode<VDom, Node> {
       kind: VWidget(attr, type)
     });
 
-  public function mountInto(target:Node) {
-    var root = new VRoot(target, [this], new DomDiffer());
-  }
+  public function renderInto(target:Node) 
+    differ.render([this], target);
+
+  static var differ = new DomDiffer();
 
 }
+
 
 private class DomDiffer extends Differ<VDom, js.html.Node> {
 
   public function new() {}
 
-  override function create(tag:String, vdom:VDom, root, parent):js.html.Node 
+  override function getLastRender(target:Node):Null<Rendered<VDom, Node>> 
+    return untyped target._coco_;
+
+  override function setLastRender(target:Node, r:Rendered<VDom, Node>) 
+    untyped target._coco_ = r;
+
+  override function setChildren(target:Node, children:Array<Node>) {
+    var e:Element = cast target;
+    if (children == null) e.innerHTML = '';
+    else {
+      var pos = 0;
+      for (nu in children) {
+        var old = e.childNodes[pos++];
+        if (old != nu) 
+          e.insertBefore(nu, old);
+      }
+      for (i in pos...e.childNodes.length)
+        e.removeChild(e.childNodes[pos]);
+    }
+  }
+
+  override function updateNative(real:Node, nu:VDom, old:VDom, parent:Null<Parent<VDom, Node>>, later:Later) 
+    if (real.nodeType == Node.TEXT_NODE) {
+      var text = nu.attributes.text;
+      if (text != old.attributes.text) real.nodeValue = text;
+    }
+    else {
+      var elt:Element = cast real;
+      updateObject(elt, nu.attributes, old.attributes, setProp);
+      _render(cast nu.children, elt, parent, later);
+    }
+
+  override function createNative(tag:NodeType, vdom:VDom, parent:Null<Parent<VDom, Node>>, later:Later):Node 
     return switch tag {
       case '': 
         document.createTextNode(vdom.attributes.text);
       case other: 
         var elt = document.createElement(tag);
         updateObject(elt, vdom.attributes, null, setProp);
-        switch vdom.children {
-          case null:
-          case c:
-            var rendered = renderAll(cast c, root, parent);
-            untyped elt._coco_rendered = rendered;
-            for (node in flatten(rendered.childList))
-              elt.appendChild(node);
-        }
+        _render(cast vdom.children, elt, parent, later);
         elt;
     }
 
+  override function destroyNative(n:Node) {
+    switch getLastRender(n) {
+      case null:
+      case r:
+        for (c in r.childList)
+          destroyRender(c);
+    }
+    switch n.parentNode {
+      case null:
+      case p: p.removeChild(n);
+    }
+  }
+
+  override function removeChild(real:Node, child:Node) 
+    real.removeChild(child);
+
   override function nativeParent(node:Node)
     return node.parentNode;
-
-  override function updateNative(node:Node, tag:String, nu:VDom, old:VDom, root, parent) 
-    if (tag == '') {
-      var text = nu.attributes.text;
-      if (text != old.attributes.text)
-        node.textContent = text;
-    }
-    else {
-      var elt:Element = cast node;
-      if (old.children.length + nu.children.length > 0) {
-        var o:{ _coco_rendered: Rendered<VDom, Node> } = cast node;
-        updateObject(elt, nu.attributes, old.attributes, setProp);
-        o._coco_rendered = updateAll(o._coco_rendered, cast nu.children, root, parent);
-      }
-    }
 
   inline function setProp(element:Element, name:String, newVal:Dynamic, ?oldVal:Dynamic)
     switch name {
@@ -101,45 +130,4 @@ private class DomDiffer extends Differ<VDom, js.html.Node> {
   inline function updateAttribute(element:Element, name:String, newVal:Dynamic, oldVal:Dynamic) 
     if (newVal == null) element.removeAttribute(name);
     else element.setAttribute(name, newVal);
-  
-  override function spliceChildren(target:js.html.Node, children:Array<js.html.Node>, start:js.html.Node, oldCount:Int) {
-    if (untyped target.className == 'todo-list') {
-      trace(target);
-      trace(children.length);
-    }
-    var pos = 
-      if (start == null) 0;
-      else {
-        var found = -1;
-        for (i in 0...target.childNodes.length)
-          if (target.childNodes[i] == start) {
-            found = i;
-            break;
-          }
-        if (found == -1) throw 'start node not found';
-        0;
-      }
-
-    var created = 0;
-
-    function add(nu:js.html.Node) {
-      var old = target.childNodes[pos];
-      if (old != nu) {
-        if (nu.parentNode == null) created++;
-        target.insertBefore(nu, old);
-      }
-      pos++;
-    }
-    
-    for (c in children)
-      add(c);
-
-    // trace(oldCount + created - children.length);
-
-    for (i in 0...oldCount + created - children.length)
-      target.removeChild(target.childNodes[pos]);
-  }
-
-  override function setChildren(target:js.html.Node, children:Array<js.html.Node>) 
-    spliceChildren(target, children, target.childNodes[0], target.childNodes.length);
 }
