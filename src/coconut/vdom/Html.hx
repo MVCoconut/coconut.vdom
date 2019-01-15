961840dc21d25e3c1a01619e73138cc0734ff4ea
Juraj Kirchheim <juraj.kirchheim@doclerholding.com>
2019-01-15 09:55:17 +0100

    Add back support for raw html.

--- src/coconut/vdom/Html.hx
+++ src/coconut/vdom/Html.hx
@@ -6,4 +6,33 @@ import js.html.*;
 class Html {
   static inline function h(tag:String, attr:Dynamic, ?children:coconut.ui.Children):Child 
     return @:privateAccess Child.element(tag, attr, cast children);
+
+  static public inline function raw(attr):Child
+    return HtmlFragment.fromHxx(attr);
+}
+
+private class HtmlFragment extends coconut.ui.View {
+  @:attribute var content:String;
+  @:attribute var tag:String = 'span'; 
+  @:attribute var className:tink.domspec.ClassName = null;
+  
+  var root:Element;
+  var lastTag:String;
+  var lastContent:String;
+
+  function render()
+    return @:privateAccess Html.h(tag, { className: className, ref: function (e) this.root = e });
+
+  function viewDidMount() {
+    lastContent = tag;
+    root.innerHTML = lastContent = content;
+  }
+
+  function viewDidUpdate() 
+    if (lastContent != content || lastTag != tag) {
+      root.innerHTML = content;
+      lastContent = content;
+      lastTag = tag;
+    }
+    
 }
