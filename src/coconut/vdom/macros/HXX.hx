2c17b48379f82d7ed9f42563d0d80fe81ee395ce
Juraj Kirchheim <back2dos@googlemail.com>
2020-04-22 15:25:27 +0200

    Remove a bit of noise.

--- src/coconut/vdom/macros/HXX.hx
+++ src/coconut/vdom/macros/HXX.hx
@@ -3,14 +3,11 @@ package coconut.vdom.macros;
 #if macro
 import coconut.ui.macros.Helpers;
 import tink.hxx.*;
-import haxe.macro.Expr;
-
-using tink.MacroApi;
 
 class HXX {
   static final generator = new Generator(Tag.extractAllFrom(macro coconut.vdom.Html));
 
-  static public function parse(e:Expr)
+  static public function parse(e)
     return Helpers.parse(e, generator, 'coconut.diffing.VNode.fragment');
 }
 #end
